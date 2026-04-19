05947bf5aeff673977247b143d4afb8e2ceff5dd
Kaio Vinícios <kvsgpro@outlook.com>
2026-04-19 17:12:01 -0300

    refactor(request): change Request model to use TimeStampedModel and remove all migrations

--- apps/request/models.py
+++ apps/request/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 from .choices import REQUEST_TYPES
-from django.contrib.auth.models import User
 from apps.supply.models import Supply
+from django.contrib.auth.models import User
+from apps.core.models import TimeStampedModel
 
 
-class Request(models.Model):
+class Request(TimeStampedModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="request")
     request_type = models.CharField(
         max_length=20,
         choices=REQUEST_TYPES,
     )
     supply = models.ForeignKey(Supply, on_delete=models.PROTECT, related_name="request")
-    description = models.TextField()
-    request_date = models.DateField(auto_now_add=True)
+    description = models.TextField(max_length=100)
     is_approved = models.BooleanField(default=False)
     approval_date = models.DateField(null=True, blank=True)
     quantity = models.FloatField()
