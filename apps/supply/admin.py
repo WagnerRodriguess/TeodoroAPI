31c41b99183e99a722039ea0dc6197f763eb9347
Wagner <wwagnerrrodriguess@gmail.com>
2026-04-21 16:56:54 -0300

    fix(supply): alterando forma de registro no admin

--- apps/supply/admin.py
+++ apps/supply/admin.py
@@ -2,8 +2,4 @@ from django.contrib import admin
 
 from apps.supply.models import Supply
 
-@admin.register(Supply)
-class SupplyAdmin(admin.ModelAdmin):
-    list_display = ["supply_label", "status", "quantity", "unit_of_measure"]
-    search_fields = ["supply_label__name", "description"]
-    list_filter = ["status"]
+admin.site.register(Supply)
